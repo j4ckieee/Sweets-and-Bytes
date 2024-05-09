7d767b7868682f43bf150495855943fd06fc3b43
j4ckieee <146953876+j4ckieee@users.noreply.github.com>
2024-05-08 18:41:00 -0700

    Add files via upload

--- public/js/add_product.js
+++ public/js/add_product.js
@@ -1,35 +1,35 @@
 // Get the objects we need to modify
-let addCustomerForm = document.getElementById('add-customer-form-ajax');
+let addProductForm = document.getElementById('add-product-form-ajax');
 
 // Modify the objects we need
-addCustomerForm.addEventListener("submit", function (e) {
+addProductForm.addEventListener("submit", function (e) {
     
     // Prevent the form from submitting
     e.preventDefault();
 
     // Get form fields we need to get data from
-    let inputFirstName = document.getElementById("input-first_name");
-    let inputLastName = document.getElementById("input-last_name");
-    let inputEmail = document.getElementById("input-email");
-    let inputPhoneNumber = document.getElementById("input-phone_number");
+    let inputProductName = document.getElementById("input-product_name");
+    let inputProductDescription = document.getElementById("input-product_description");
+    let inputProductPrice = document.getElementById("input-product_price");
+    let inputProductInventory = document.getElementById("input-product_inventory");
 
     // Get the values from the form fields
-    let firstNameValue = inputFirstName.value;
-    let lastNameValue = inputLastName.value;
-    let emailValue = inputEmail.value;
-    let phoneNumberValue = inputPhoneNumber.value;
+    let productNameValue = inputProductName.value;
+    let productDescriptionValue = inputProductDescription.value;
+    let productPriceValue = inputProductPrice.value;
+    let productInventoryValue = inputProductInventory.value;
 
     // Put our data we want to send in a javascript object
     let data = {
-        first_name: firstNameValue,
-        last_name: lastNameValue,
-        email: emailValue,
-        phone_number: phoneNumberValue
+        product_name: productNameValue,
+        product_description: productDescriptionValue,
+        product_price: productPriceValue,
+        product_inventory: productInventoryValue
     }
     
     // Setup our AJAX request
     var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "/add-customer-ajax", true);
+    xhttp.open("POST", "/add-product-ajax", true);
     xhttp.setRequestHeader("Content-type", "application/json");
 
     // Tell our AJAX request how to resolve
@@ -40,10 +40,10 @@ addCustomerForm.addEventListener("submit", function (e) {
             addRowToTable(xhttp.response);
 
             // Clear the input fields for another transaction
-            inputFirstName.value = '';
-            inputLastName.value = '';
-            inputEmail.value = '';
-            inputPhoneNumber.value = '';
+            inputProductName.value = '';
+            inputProductDescription.value = '';
+            inputProductPrice.value = '';
+            inputProductInventory.value = '';
         }
         else if (xhttp.readyState == 4 && xhttp.status != 200) {
             console.log("There was an error with the input.")
@@ -61,7 +61,7 @@ addCustomerForm.addEventListener("submit", function (e) {
 addRowToTable = (data) => {
 
     // Get a reference to the current table on the page and clear it out.
-    let currentTable = document.getElementById("customer-table");
+    let currentTable = document.getElementById("product-table");
 
     // Get the location where we should insert the new row (end of table)
     let newRowIndex = currentTable.rows.length;
@@ -73,24 +73,24 @@ addRowToTable = (data) => {
     // Create a row and 4 cells
     let row = document.createElement("TR");
     //let idCell = document.createElement("TD");
-    let firstNameCell = document.createElement("TD");
-    let lastNameCell = document.createElement("TD");
-    let emailCell = document.createElement("TD");
-    let phoneNumberCell = document.createElement("TD");
+    let productNameCell = document.createElement("TD");
+    let productDescriptionCell = document.createElement("TD");
+    let productPriceCell = document.createElement("TD");
+    let productInventoryCell = document.createElement("TD");
 
     // Fill the cells with correct data
     //idCell.innerText = newRow.id;
-    firstNameCell.innerText = newRow.first_name;
-    lastNameCell.innerText = newRow.last_name;
-    emailCell.innerText = newRow.email;
-    phoneNumberCell.innerText = newRow.phone_number;
+    productNameCell.innerText = newRow.product_name;
+    productDescriptionCell.innerText = newRow.product_description;
+    productPriceCell.innerText = newRow.product_price;
+    productInventoryCell.innerText = newRow.product_inventory;
 
     // Add the cells to the row 
     //row.appendChild(idCell);
-    row.appendChild(firstNameCell);
-    row.appendChild(lastNameCell);
-    row.appendChild(emailCell);
-    row.appendChild(phoneNumberCell);
+    row.appendChild(productNameCell);
+    row.appendChild(productDescriptionCell);
+    row.appendChild(productPriceCell);
+    row.appendChild(productInventoryCell);
     
     // Add the row to the table
     currentTable.appendChild(row);
