3cd2358da4b93be4cbe3a9b9ab5fc5e0c2daf340
jktruongg <137835728+jktruongg@users.noreply.github.com>
2024-06-10 18:08:43 -0700

    added readme file, added/updated citations
    small form description update

--- database/db-connector.js
+++ database/db-connector.js
@@ -7,9 +7,9 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_tramn',
-    password        : '1386',
-    database        : 'cs340_tramn'
+    user            : '----',
+    password        : '----',
+    database        : '----'
 })
 
 // Export it for use in our applicaiton
